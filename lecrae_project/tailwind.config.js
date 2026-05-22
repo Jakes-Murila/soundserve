3cca31e93ec8df1aa96a90dc8b42062557d082d2
Jakes-Murila <murilajakes0@gmail.com>
2026-05-22 09:38:40 +0300

    configs

---
+++ lecrae_project/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#050505",
+        card: "#111111",
+        accent: "#9eff00",
+      },
+    },
+  },
+  plugins: [],
+}
